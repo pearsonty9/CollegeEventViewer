dccc85f8b4e67ed5b405238966df3ba314036a23
Tyler Pearson <pearsonty9@gmail.com>
2022-02-08 14:44:11 -0500

    feat: login screen

---
+++ web/src/screens/Login.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { Typography, Button, setRef } from '@mui/material';
+
+function Login(props) {
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+        showPassword: false,
+    });
+
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+    };
+
+    const handleClickShowPassword = () => {
+        setValues({
+            ...values,
+            showPassword: !values.showPassword,
+        });
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const handelSignIn = (event) => {
+        event.preventDefault();
+        // call api
+        // handle errors
+        if (values.email != 'email' || values.password != 'password') {
+            setError(true);
+            setErrorMessage("incorrect username or password");
+        }
+        else {
+            // login in user
+            setError(false);
+            setErrorMessage("logged in user");
+        }
+    }
+    
+      return (
+        <form>
+            <Box sx={{ display: 'flex', flexDirection: 'column' , padding: 1, width: {xs: '35ch', sm: '50%', md: '50ch'}, my: 20, mx: 'auto' }}>
+                <Typography variant='h3' sx={{ my: 2 }}>Login</Typography>
+                <TextField id="email-field" error={error} label="Email" onChange={handleChange('email')} fullWidth sx={{ marginY: 1 }}/>
+                <FormControl sx={{ width: '100%', marginY: 1 }} variant="outlined">
+                <InputLabel htmlFor="password-field-label" error={error}>Password</InputLabel>
+                <OutlinedInput
+                    id="password-field"
+                    error={error}
+                    type={values.showPassword ? 'text' : 'password'}
+                    value={values.password}
+                    onChange={handleChange('password')}
+                    endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                        >
+                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                    </InputAdornment>
+                    }
+                    label="Password"
+                />
+                <FormHelperText id="login-helper-text" error={error}>{errorMessage}</FormHelperText>
+                </FormControl>
+                <Button variant="contained" type="submit" size="medium" onClick={handelSignIn} sx={{ width: '30%', alignSelf: "flex-end" }}>Sign in</Button>
+            </Box>
+        </form>
+      );
+}
+
+export default Login;
